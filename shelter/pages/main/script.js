9398e252e42820f4e26229376352ecba20a3af52
Nika Perfilova <ees2009.2014@gmail.com>
2022-04-18 22:37:31 +0700

    feat: add hamburger menu

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,2 +1,23 @@
 import 'normalize.css';
 import './style.scss';
+import { handleNav, toggleNav } from 'js/navigation';
+
+const nav = document.querySelector('.header__nav');
+const burgerButton = document.querySelector('.header__burger');
+
+const handleMediaQuery = (e) => {
+  console.log(e);
+  if (e.matches) {
+    burgerButton.addEventListener('click', () => toggleNav(nav, burgerButton));
+    nav.addEventListener('click', (e) => handleNav(e, nav, burgerButton));
+  }
+};
+
+const init = () => {
+  const mediaQuery = window.matchMedia('(max-width: 767px)');
+
+  mediaQuery.addEventListener('change', handleMediaQuery);
+  handleMediaQuery(mediaQuery);
+};
+
+window.addEventListener('load', init);
--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -1,1 +1,36 @@
+import 'normalize.css';
 import './style.scss';
+import { handleNav, toggleNav } from 'js/navigation';
+
+const nav = document.querySelector('.header__nav');
+const burgerButton = document.querySelector('.header__burger');
+const navLinks = nav.querySelectorAll('.link');
+
+const handleMediaQuery = (e) => {
+  console.log(e);
+  if (e.matches) {
+    burgerButton.addEventListener('click', () => toggleNav(nav, burgerButton));
+    nav.addEventListener('click', (e) => handleNav(e, nav, burgerButton));
+
+    navLinks.forEach((link) => {
+      link.classList.remove('link--dark', 'link--active-dark');
+    });
+  } else {
+    navLinks.forEach((link) => {
+      if (link.classList.contains('link--active')) {
+        link.classList.add('link--active-dark');
+      } else {
+        link.classList.add('link--dark');
+      }
+    });
+  }
+};
+
+const init = () => {
+  const mediaQuery = window.matchMedia('(max-width: 767px)');
+
+  mediaQuery.addEventListener('change', handleMediaQuery);
+  handleMediaQuery(mediaQuery);
+};
+
+window.addEventListener('load', init);
