3901e4a1fd390442c4ccf4d0e4d275cbda80cdd4
Nika Perfilova <ees2009.2014@gmail.com>
2022-04-23 21:41:16 +0700

    feat: add modal to the pets page

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -2,12 +2,14 @@ import 'normalize.css';
 import './style.scss';
 import { stopAnimationOnLoad, stopAnimationOnResize } from 'assets/js/stopAnimation';
 import { initNav } from 'assets/js/navigation';
+import { initModal } from '../../assets/js/modal';
 import { initPagination } from '../../assets/js/pagination';
 
 const init = () => {
   stopAnimationOnLoad();
   stopAnimationOnResize();
   initNav();
+  initModal();
   initPagination();
 };
 
